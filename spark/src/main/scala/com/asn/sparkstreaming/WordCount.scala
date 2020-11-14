0ee2bdd6f299f521a8a2ad0af868f3eac9ef9051
wangsen <895409039@qq.com>
2020-11-14 18:19:12 +0800

    udafdemo1 和 spark streaming wordcount

---
+++ spark/src/main/scala/com/asn/sparkstreaming/WordCount.scala
@@ -0,0 +1,12 @@
+package com.asn.sparkstreaming
+
+/**
+ * @Author: wangsen
+ * @Date: 2020/11/14 18:16
+ * @Description: 使用netcat产生数据，sparkstreaming实时读取进行单词统计
+ **/
+object WordCount {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
