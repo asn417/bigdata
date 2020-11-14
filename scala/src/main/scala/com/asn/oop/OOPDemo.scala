387a94614243f315b1162db031d6429d4529895a
“e <895409039@qq.com>
2020-11-14 10:32:01 +0800

    scala oop

---
+++ scala/src/main/scala/com/asn/oop/OOPDemo.scala
@@ -0,0 +1,19 @@
+package com.asn.oop
+
+object OOPDemo {
+  def main(args: Array[String]): Unit = {
+    val user: User = new User()
+    user.userName="zhangsan"
+    println(user.userName)
+  }
+}
+
+class User{
+  var userName:String=_
+  var age:Int=_
+  def login():Boolean={
+    true
+  }
+}
+
+
