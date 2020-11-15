80e905a36061cc8a6e21cfb156fa7e4cbe88d304
wangsen <895409039@qq.com>
2020-11-15 12:00:15 +0800

    自定义receiver

---
+++ spark/src/main/scala/com/asn/spark/receiver/MyReceiver.scala
@@ -0,0 +1,43 @@
+package com.asn.spark.receiver
+
+import java.io.{BufferedReader, InputStreamReader}
+import java.net.Socket
+
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.receiver.Receiver
+
+/**
+ * @Author: wangsen
+ * @Date: 2020/11/15 11:19
+ * @Description: 自定义采集器
+ **/
+class MyReceiver(host:String,port:Int) extends Receiver[String](StorageLevel.MEMORY_ONLY){
+  override def onStart(): Unit = {
+    new Thread(() => {
+      receive()
+    }).start()
+  }
+  //socket对象没有继承Serializable，不可序列化，因此创建socket对象要在receive中写，也就相当于每个task都创建了一个socket
+  private var socket:java.net.Socket =null
+  def receive(): Unit ={
+    socket =  new java.net.Socket(host, port)
+    val reader = new BufferedReader(new InputStreamReader(socket.getInputStream,"UTF-8"))
+    var line:String = null
+
+    while ((line = reader.readLine()) != null){
+      if ("END".equals(line)){
+        return
+      }else{
+        //将采集的数据存储到采集器的内部进行封装转换
+        this.store(line)
+      }
+    }
+
+  }
+  override def onStop(): Unit = {
+    if (socket!= null){
+      socket.close()
+      socket=null
+    }
+  }
+}
---
+++ spark/src/main/scala/com/asn/sparkstreaming/WordCountWithMyReceiver.scala
@@ -0,0 +1,31 @@
+package com.asn.sparkstreaming
+
+import com.asn.spark.receiver.MyReceiver
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
+
+/**
+ * @Author: wangsen
+ * @Date: 2020/11/15 11:38
+ * @Description:
+ **/
+object WordCountWithMyReceiver {
+  def main(args: Array[String]): Unit = {
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("streaming-wordcount-withMyReceiver")
+
+    //每三秒为一批数据，因此每次统计的都是这5秒内的数据
+    val streamingContext: StreamingContext = new StreamingContext(sparkConf,Seconds(3))
+
+    val socketStreaming: ReceiverInputDStream[String] = streamingContext.receiverStream(new MyReceiver("flink1",9999))
+
+    socketStreaming.flatMap((line: String) =>line.split(" "))
+      .map(word=>(word,1))
+      .reduceByKey(_+_)
+      .print()
+
+    streamingContext.start()
+
+    streamingContext.awaitTermination()
+  }
+}
