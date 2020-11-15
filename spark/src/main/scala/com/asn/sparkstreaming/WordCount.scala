c25ce49c986ba2b69e6ef216fdbf21a9a59411c8
wangsen <895409039@qq.com>
2020-11-15 11:14:32 +0800

    sparkstreaming wordcount

--- spark/src/main/scala/com/asn/sparkstreaming/WordCount.scala
+++ spark/src/main/scala/com/asn/sparkstreaming/WordCount.scala
@@ -1,5 +1,9 @@
 package com.asn.sparkstreaming
 
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 /**
  * @Author: wangsen
  * @Date: 2020/11/14 18:16
@@ -7,6 +11,21 @@ package com.asn.sparkstreaming
  **/
 object WordCount {
   def main(args: Array[String]): Unit = {
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("streaming-wordcount")
+
+    //每三秒为一批数据，因此每次统计的都是这3秒内的数据
+    val streamingContext: StreamingContext = new StreamingContext(sparkConf,Seconds(3))
+
+    val socketStreaming: ReceiverInputDStream[String] = streamingContext.socketTextStream("flink1",9999)
+
+    socketStreaming.flatMap(line=>line.split(" "))
+      .map(word=>(word,1))
+      .reduceByKey(_+_)
+      .print()
+
+    streamingContext.start()
+
+    streamingContext.awaitTermination()
 
   }
 }
