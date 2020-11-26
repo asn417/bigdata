ff673473fdac1997ce964ff0015a09e7bfb6a9d7
wangsen <895409039@qq.com>
2020-11-27 01:44:24 +0800

    sparksql 写数据到hive表

--- spark/src/main/scala/com/asn/sparkSQL/SparkSqlHive.scala
+++ spark/src/main/scala/com/asn/sparkSQL/SparkSqlHive.scala
@@ -1,18 +1,40 @@
 package com.asn.sparkSQL
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 /**
- * sparkSQL可以通过hive-site.xml文件的配置直接读取hive元数据.
- * 通过enableHiveSupport()方法开启hive支持，前提是需要将hive-site.xml拷贝到spark的配置目录下
+ * 提交到yarn运行
+ * spark-submit --master yarn --class com.asn.sparkSQL.SparkSqlHive spark-1.0-SNAPSHOT.jar
  */
 object SparkSqlHive {
   def main(args: Array[String]): Unit = {
-    val spark: SparkSession = SparkSession.builder().master("local[*]").appName("Hive on Spark")
-      .enableHiveSupport().getOrCreate()
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Hive on Spark")
+      .config("spark.sql.warehouse.dir","hdfs://ns1/user/hive")
+      .config("hive.metastore.uris","thrift://flink3:9083")
+      .enableHiveSupport()
+      .getOrCreate()
 
-    spark.sql("").show()
+    val schema = StructType(
+      List(
+        StructField("name",StringType),
+        StructField("age",IntegerType),
+      )
+    )
 
-    spark.stop()
+    //读取数据到DF
+    val df: DataFrame = spark.read.option("delimiter", ",")
+      .schema(schema)
+      .csv("hdfs:///student.csv")
+
+    import spark.implicits._
+
+    //过滤数据
+    val resultDF = df.where('age>30)
+
+    //将数据存到spark数据库的student表
+    resultDF.write.mode(SaveMode.Overwrite).saveAsTable("spark.student")
   }
 }
