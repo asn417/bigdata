e3a78cd3edb2614175f22c3c1224aba8ba06fd9b
wangsen <895409039@qq.com>
2020-11-28 17:29:08 +0800

    withcolumn增加列修改列

--- spark/src/main/scala/com/asn/spark/demo/JoinDemo.scala
+++ spark/src/main/scala/com/asn/spark/demo/JoinDemo.scala
@@ -1,4 +1,4 @@
-package com.asn.demo
+package com.asn.spark.demo
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
--- spark/src/main/scala/com/asn/spark/demo/MapDemo.scala
+++ spark/src/main/scala/com/asn/spark/demo/MapDemo.scala
@@ -1,4 +1,4 @@
-package com.asn.demo
+package com.asn.spark.demo
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
