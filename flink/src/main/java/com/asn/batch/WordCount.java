21a22a3ba16584c9d64a23b4aa49a972ea68a3e1
wangsen <895409039@qq.com>
2020-12-06 18:02:41 +0800

    flink dataset wordcount

---
+++ flink/src/main/java/com/asn/batch/WordCount.java
@@ -0,0 +1,39 @@
+package com.asn.batch;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataSource<String> source = env.readTextFile("D:\\ideaProject\\bigdata\\flink\\src\\main\\resources\\data\\word.txt");
+        //DataSet<String> source = env.readTextFile("D:\\ideaProject\\bigdata\\flink\\src\\main\\resources\\data\\word.txt");
+
+        AggregateOperator<Tuple2<String, Integer>> sum = source.flatMap(new MyFlatMapper()).groupBy(0).sum(1);
+        //DataSet<Tuple2<String, Integer>> sum = source.flatMap(new MyFlatMapper()).groupBy(0).sum(1);
+
+        sum.print();
+    }
+
+    public static class MyFlatMapper implements FlatMapFunction<String, Tuple2<String, Integer>>{
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+            String[] words = value.split(" ");
+            for (String word :
+                    words) {
+                Tuple2<String, Integer> t = new Tuple2<>();
+                t.f0 = word;
+                t.f1 = 1;
+                out.collect(t);
+            }
+        }
+    }
+
+}
