c3520fa6ac3fe512dd412699067cf40c8051b68b
wangsen <895409039@qq.com>
2021-01-09 13:14:48 +0800

    coalesce和repartition算子

--- spark/src/main/scala/com/asn/spark/rdd/CoalesceDemo.scala
+++ spark/src/main/scala/com/asn/spark/rdd/CoalesceDemo.scala
@@ -4,7 +4,8 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 /*
 coalesce算子能缩减分区，但默认是不会进行shuffle的，也就是说缩减分区的方式是将若干个分区整体合并为一个分区，而不会将一个分区打散后放到多个分区。
-因此，如果使用coalesce算子进行扩大分区的话，必须指定shuffle。否则没有意义（不会扩大分区）
+因此，如果使用coalesce算子进行扩大分区的话，必须指定shuffle。否则没有意义（不会扩大分区）。因此，spark又提供了一个repartition算子来实现
+扩大分区，底层就是用的coalesce。
  */
 object CoalesceDemo {
 
@@ -16,8 +17,8 @@ object CoalesceDemo {
     //value.saveAsTextFile("spark\\src\\main\\resources\\data\\output")
     //val shuffleRDD: RDD[Int] = value.coalesce(2)
     //val shuffleRDD: RDD[Int] = value.coalesce(2, true)
-    val shuffleRDD: RDD[Int] = value.coalesce(4, true)
-
+    //val shuffleRDD: RDD[Int] = value.coalesce(4, true)
+    val shuffleRDD: RDD[Int] =  value.repartition(4);
     shuffleRDD.saveAsTextFile("spark\\src\\main\\resources\\data\\output")
 
   }
