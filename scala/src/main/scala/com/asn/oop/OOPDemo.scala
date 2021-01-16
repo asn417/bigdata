e853c8a2330b1f05d09a96a139b0eab0a1adba2f
wangsen <895409039@qq.com>
2021-01-16 17:26:43 +0800

    oop

--- scala/src/main/scala/com/asn/oop/OOPDemo.scala
+++ scala/src/main/scala/com/asn/oop/OOPDemo.scala
@@ -3,18 +3,23 @@ package com.asn.oop
 object OOPDemo {
   def main(args: Array[String]): Unit = {
     val user: User = new User()
+    //这就是setter
     user.userName="zhangsan"
+    //这就是getter
     println(user.userName)
 
   }
 }
 
 class User{
+  //属性的默认修饰符是private，且Scala会自动提供公共的getter，setter方法
+  //如果显示的使用private修饰属性，则编译后生成的getter和setter方法也是private的
   var userName:String=_
   var age:Int=_
   def login():Boolean={
     true
   }
+  //如果属性用val修饰，那么编译后就是private final修饰的属性，且只提供getter方法
 }
 
 
