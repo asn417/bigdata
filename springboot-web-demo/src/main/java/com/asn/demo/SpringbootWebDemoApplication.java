4082168389570ec7d380665f7ef25da0a55632d8
wangsen <895409039@qq.com>
2021-03-25 11:45:31 +0800

    springboot web demo

---
+++ springboot-web-demo/src/main/java/com/asn/demo/SpringbootWebDemoApplication.java
@@ -0,0 +1,12 @@
+package com.asn.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebDemoApplication.class, args);
+    }
+}
