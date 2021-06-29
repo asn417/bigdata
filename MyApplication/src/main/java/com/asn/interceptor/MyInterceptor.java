c728fea82c5f512ffdcb5f6b3eca140ef60628ec
王森16 <wangsen16@hikvision.com.cn>
2021-06-29 14:39:39 +0800

    拦截器

--- MyApplication/src/main/java/com/asn/interceptor/MyInterceptor.java
+++ MyApplication/src/main/java/com/asn/interceptor/MyInterceptor.java
@@ -14,13 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class MyInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("=====preHandle");
+        System.out.println("拦截器=====preHandle");
         return true;
     }
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        System.out.println("=====postHandle");
+        System.out.println("拦截器=====postHandle");
     }
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-        System.out.println("=======afterCompletion");
+        System.out.println("拦截器=======afterCompletion");
     }
 }
